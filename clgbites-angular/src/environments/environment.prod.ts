fdbd498412b1f7dfe9603fd9d3c84345bc058c06
Brahma Teja <parabrahmateja@gmail.com>
2026-03-08 12:10:02 +0530

    add supabase config

--- clgbites-angular/src/environments/environment.prod.ts
+++ clgbites-angular/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   supabase: {
-    url: 'YOUR_SUPABASE_PROJECT_URL',
-    anonKey: 'YOUR_SUPABASE_ANON_KEY',
+    url: 'https://rnkuzdanyvncgwwnwbzc.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJua3V6ZGFueXZuY2d3d253YnpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5MzE3MDQsImV4cCI6MjA4ODUwNzcwNH0.N17J-hxPcNgcsjIRzxQ4uyfd0bl-ITNLaR-zpBUi2V4',
   },
   adminPassword: 'clgbites@admin2024',
 };
--- clgbites-angular/src/environments/environment.ts
+++ clgbites-angular/src/environments/environment.ts
@@ -9,6 +9,7 @@ export const environment = {
   production: false,
   supabase: {
     url: 'https://rnkuzdanyvncgwwnwbzc.supabase.co',        // e.g. https://abcxyz.supabase.co
+
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJua3V6ZGFueXZuY2d3d253YnpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5MzE3MDQsImV4cCI6MjA4ODUwNzcwNH0.N17J-hxPcNgcsjIRzxQ4uyfd0bl-ITNLaR-zpBUi2V4',       // long JWT key from Settings → API
   },
   adminPassword: 'clgbites@admin2024',        // change this to your own password
