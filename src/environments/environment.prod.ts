535bb30f0ba3043c87873766061b3e94041920bc
Brahma Teja <parabrahmateja@gmail.com>
2026-03-11 02:40:33 +0530

    restore connection

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  supabase: {
+    url: 'https://rnkuzdanyvncgwwnwbzc.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJua3V6ZGFueXZuY2d3d253YnpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5MzE3MDQsImV4cCI6MjA4ODUwNzcwNH0.N17J-hxPcNgcsjIRzxQ4uyfd0bl-ITNLaR-zpBUi2V4',
+  },
+  adminPassword: 'clgbites@admin2024',
+};
